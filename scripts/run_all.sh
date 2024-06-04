8a50f6a2fe675d131e395f7a6fc485b8c9a0cdc0
chengshuang18 <60605542+chengshuang18@users.noreply.github.com>
2024-06-04 18:24:31 +0800

    Update run_all.sh

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,7 +1,7 @@
-source /workspace/S/zhangyang/miniconda3/bin/activate cluster_lora
-conda info -e
-cd /lustre/S/zhangyang/chengshuang/CL/cluster_activate_lora
-export NCCL_DEBUG=INFO
+# source /workspace/S/zhangyang/miniconda3/bin/activate cluster_lora
+# conda info -e
+# cd /lustre/S/zhangyang/chengshuang/CL/cluster_activate_lora
+# export NCCL_DEBUG=INFO
 
 # echo "bash scripts/run_order1.sh"
 # bash scripts/run_order1.sh
